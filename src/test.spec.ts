503868fdfe6b39316198f7c6ee0280b9ee144ba8
Jonny <jmwright@scottlogic.com>
2020-08-11 11:58:13 +0100

    See what happens if the tests fail

--- src/test.spec.ts
+++ src/test.spec.ts
@@ -1,7 +1,7 @@
 describe('unit test placeholder', () => {
   it('should run a test', () => {
     const actual = 'hello' + ' ' + 'world'
-    const expected = 'hello world'
+    const expected = 'hello world asdfasdfasdf'
     expect(actual).toEqual(expected)
   })
 })
