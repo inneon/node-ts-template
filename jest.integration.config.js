2d623f4d467c12633605835d214640c4eff3e5c4
jonny <jonathon.wright@converge.io>
2021-11-10 10:55:04 +0000

    remove deliberately failing tests

--- jest.integration.config.js
+++ jest.integration.config.js
@@ -4,5 +4,6 @@ module.exports = {
   modulePathIgnorePatterns: ['lib'],
   testEnvironment: 'node',
   testMatch: ['<rootDir>/src/integration/*.spec.ts'],
+  setupFiles: ['./src/tests/jest.overrides.ts'],
   preset: 'ts-jest',
 }
