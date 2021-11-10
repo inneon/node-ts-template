d39945cc742d66d65fc36597f12a684e5ae01029
jonny <jonathon.wright@converge.io>
2021-11-10 10:50:41 +0000

    fail test runs on console error or warning

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,7 @@ module.exports = {
   clearMocks: true,
   coverageProvider: 'v8',
   modulePathIgnorePatterns: ['lib', 'src/integration'],
+  setupFiles: ['./src/tests/jest.overrides.ts'],
   testEnvironment: 'node',
   preset: 'ts-jest',
 }
